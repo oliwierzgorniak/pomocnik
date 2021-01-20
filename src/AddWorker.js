355e2a9349b5569b628a503a63e507639bbe905d
Oliwier Zgórniak <oliwier.zgorniak@gmail.com>
2021-01-20 20:35:15 +0100

    styled window

--- src/AddWorker.js
+++ src/AddWorker.js
@@ -1,17 +1,15 @@
 import './AddWorker.css';
-<<<<<<< HEAD
 import './Window.css';
-=======
-import './Message.css';
->>>>>>> 5f1708d76c6c856f6c612540232677f50c0eb62e
 import more from './img/more.svg';
 import less from './img/less.svg';
 
 const AddWorker = props => {
   return (
     <div className="window">
-      <label for="buttons">Mnożnik: {props.workerMultiplyer}</label>
-      <div name="buttons">
+      <label for="buttons">
+        Mnożnik: <br /> <small>{props.workerMultiplyer}</small>
+      </label>
+      <div className="buttons" name="buttons">
         <button>
           <img src={less} alt="less"></img>
         </button>
@@ -19,8 +17,10 @@ const AddWorker = props => {
           <img src={more} alt="more"></img>
         </button>
       </div>
-      <label>Pensja: {props.workerSalary}</label>
-      <div name="buttons">
+      <label>
+        Pensja: <br /> <small>{props.workerSalary}</small>
+      </label>
+      <div className="buttons" name="buttons">
         <button>
           <img src={less} alt="less"></img>
         </button>
@@ -28,8 +28,10 @@ const AddWorker = props => {
           <img src={more} alt="more"></img>
         </button>
       </div>
-      <label>Ubezpieczenie: {props.workerInsurance}</label>
-      <div name="">
+      <label>
+        Ubezpieczenie: <br /> <small>{props.workerInsurance}</small>
+      </label>
+      <div className="buttons" name="">
         <button>
           <img src={less} alt="less"></img>
         </button>
